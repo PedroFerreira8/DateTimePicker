785969f8860dc6a5b5096e2fc2f8f910e821bc07
José Pedro Cunha Ferreira <jose_pedro77@hotmail.com>
2020-12-18 15:35:21 +0000

    Adicionar arquivos de projeto.

---
+++ DateTimePicker/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace DateTimePicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_CalcularIdade = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpNascimento = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // btn_CalcularIdade
+            // 
+            this.btn_CalcularIdade.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btn_CalcularIdade.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_CalcularIdade.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.btn_CalcularIdade.Location = new System.Drawing.Point(121, 200);
+            this.btn_CalcularIdade.Name = "btn_CalcularIdade";
+            this.btn_CalcularIdade.Size = new System.Drawing.Size(169, 31);
+            this.btn_CalcularIdade.TabIndex = 0;
+            this.btn_CalcularIdade.Text = "Calcular Idade Atual";
+            this.btn_CalcularIdade.UseVisualStyleBackColor = false;
+            this.btn_CalcularIdade.Click += new System.EventHandler(this.btn_CalcularIdade_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Insira a sua data de nascimento:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Calligraphy", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(75, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(255, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Calculadora de Idades";
+            // 
+            // dtpNascimento
+            // 
+            this.dtpNascimento.Location = new System.Drawing.Point(15, 127);
+            this.dtpNascimento.Name = "dtpNascimento";
+            this.dtpNascimento.Size = new System.Drawing.Size(229, 20);
+            this.dtpNascimento.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 267);
+            this.Controls.Add(this.dtpNascimento);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_CalcularIdade);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora de Idade";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_CalcularIdade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpNascimento;
+    }
+}
+
---
+++ DateTimePicker/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTimePicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_CalcularIdade_Click(object sender, EventArgs e)
+        {
+            int idade = DateTime.Now.Year - dtpNascimento.Value.Year;
+            if (DateTime.Now.DayOfYear < dtpNascimento.Value.DayOfYear) idade--;
+            MessageBox.Show("Tem " + idade + " anos de idade"); 
+        }
+    }
+}
